b649805436609f0a44069105e0c62924f80e77dc
Justin Sabelko <justin.sabelko@uscm.org>
2010-08-19 15:45:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infobase::Application.config.secret_token = '2c228bada4a92ae2a781bb58ef1857c507ab1f4aac15e6d7c4cf5e4b82caa185568165f6ad1eb5033dffcc9d8432aa0625ab7b70afa0d27782d5c6a8d1d7b8b4'
