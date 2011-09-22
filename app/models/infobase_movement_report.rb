5122c059a18dad12d902fd7a07e5fd721b331bee
Justin Sabelko <justin.sabelko@uscm.org>
2011-09-22 16:38:42 -0500

    Movement report draft #2.  Need to add date searching yet.

--- app/models/infobase_movement_report.rb
+++ app/models/infobase_movement_report.rb
@@ -2,14 +2,36 @@ class InfobaseMovementReport
   
   def self.report(type, region, date, strategies, order)
     rows = []
-    result = Activity.joins(:target_area, :team).
+    result = Activity.includes(:target_area, :team).
       where(TargetArea.table_name + ".region IN (?)", region).
       where(Activity.table_name + ".strategy IN (?)", strategies).
       where(TargetArea.table_name + ".isClosed is null OR " + TargetArea.table_name + ".isClosed = ''").
       where(Activity.table_name + ".status IN (?)", Activity.active_statuses)
+    order.each_key do |key|
+      result = result.order(translate_order(order[key])) if !order[key].blank?
+    end
     result.each do |result|
       rows << result
     end
     rows
+  end
+
+  def self.translate_order(order, type = "movement")
+    result = ""
+    case order
+    when "campus"
+      result = TargetArea.table_name + ".name"
+    when "city"
+      result = TargetArea.table_name + ".state, " + TargetArea.table_name + ".city"
+    when "team"
+      result = Team.table_name + ".name"
+    when "region"
+      result = TargetArea.table_name + ".region"
+    when "status"
+      result = Activity.table_name + ".status"
+    when "ministry"
+      result = Activity.table_name + ".strategy"
+    end
+    result
   end
 end
