abde8f0a5bb3fb5dc9f4b323964873e2cddda77b
Justin Sabelko <justin.sabelko@uscm.org>
2011-10-08 20:35:18 -0500

    Fix target area validations

--- db/migrate/20110511224501_migrate_bookmarks.rb
+++ db/migrate/20110511224501_migrate_bookmarks.rb
@@ -1,6 +1,6 @@
 class MigrateBookmarks < ActiveRecord::Migration
   def self.up
-    old_bookmarks = StaffsitePref.where("name IN ('LOCALLEVEL', 'STATISTIC', 'TARGETAREA')")
+    old_bookmarks = StaffsitePref.where("name IN ('LOCALLEVEL', 'STATISTIC', 'TARGETAREA', 'CURRENT_BALANCE')")
     old_bookmarks.each do |bookmark|
       profile = StaffsiteProfile.find_by_StaffSiteProfileID(bookmark.fk_StaffSiteProfile) if bookmark.fk_StaffSiteProfile
       user = User.find_by_username(profile.userName) if profile
@@ -10,6 +10,7 @@ class MigrateBookmarks < ActiveRecord::Migration
     execute "update infobase_bookmarks set name = 'activity' where name = 'STATISTIC'"
     execute "update infobase_bookmarks set name = 'team' where name = 'LOCALLEVEL'"
     execute "update infobase_bookmarks set name = 'target_area' where name = 'TARGETAREA'"
+    execute "update infobase_bookmarks set name = 'balance' where name = 'CURRENT_BALANCE'"
   end
 
   def self.down
