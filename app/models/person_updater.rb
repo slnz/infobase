4645e507055dc15a54738b0d191f23eb7ab8d386
Justin Sabelko <justin.sabelko@uscm.org>
2012-02-08 19:12:09 -0600

    When switching account numbers, clone and create new record, deactivate old record

--- app/models/person_updater.rb
+++ app/models/person_updater.rb
@@ -36,8 +36,7 @@ class PersonUpdater
               s_record.update_attribute(:accountNo, s_record.accountNo + "_old")
               s_record.removedFromPeopleSoft = "Y"
               s_record.save!
-              maiden_record.update_attribute(:accountNo, p_record.emplid)
-              maiden_record.save!
+              switch_account_number(maiden_record, p_record.emplid)
               staff.delete(p_record.emplid)
               Rails.logger.info("Record #{p_record.emplid} existed (#{p_record.first_name.to_s + " " + p_record.last_name.to_s}), but name didn't match (#{s_record.firstName.to_s + " " + s_record.lastName.to_s}).  Found maiden record #{maiden.maiden_emplid} and switched account numbers.")
             elsif maiden_record
@@ -45,8 +44,7 @@ class PersonUpdater
               s_record.update_attribute(:accountNo, s_record.accountNo + "_old")
               s_record.removedFromPeopleSoft = "Y"
               s_record.save!
-              maiden_record.update_attribute(:accountNo, p_record.emplid)
-              maiden_record.save!
+              switch_account_number(maiden_record, p_record.emplid)
               staff.delete(p_record.emplid)
               Rails.logger.info("Record #{p_record.emplid} existed (#{p_record.first_name.to_s + " " + p_record.last_name.to_s}), but name didn't match (#{s_record.firstName.to_s + " " + s_record.lastName.to_s}).  Found maiden record #{maiden.maiden_emplid} but name still didn't match (#{maiden_record.firstName.to_s + " " + maiden_record.lastName.to_s}).  Switched account numbers anyway.")
             else
@@ -64,8 +62,7 @@ class PersonUpdater
         if maiden && !maiden.maiden_emplid.blank? # Changed emplid at some point
           s_record = Staff.find_by_accountNo(maiden.maiden_emplid)
           if s_record
-            s_record.update_attribute(:accountNo, maiden.emplid)
-            s_record.save!
+            switch_account_number(s_record, maiden.emplid)
             staff.delete(maiden.maiden_emplid)
             Rails.logger.info("Record #{p_record.emplid} did not exist (#{p_record.first_name.to_s + " " + p_record.last_name.to_s}).  Found old record #{maiden.maiden_emplid} from maiden table and switched account numbers for staff record (#{s_record.firstName.to_s + " " + s_record.lastName.to_s}).")
           else
@@ -94,5 +91,14 @@ class PersonUpdater
         Rails.logger.info("Record #{s_record.accountNo} removed from PS.")
       end
     end
+  end
+  
+  def switch_account_number(staff, account_no)
+    new_record = staff.clone
+    staff.removedFromPeopleSoft = "Y"
+    staff.person_id = nil
+    staff.save!
+    new_record.accountNo = account_no
+    new_record.save!
   end
 end
