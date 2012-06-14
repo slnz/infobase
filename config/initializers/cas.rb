962a64a0cbf6bcdf253c6e331c30ce68e9b57afe
Josh Starcher <josh.starcher@gmail.com>
2012-06-14 09:55:45 -0400

    update cas url

--- config/initializers/cas.rb
+++ config/initializers/cas.rb
@@ -1,5 +1,5 @@
 CASClient::Frameworks::Rails3::Filter.configure(
-  :cas_base_url => "https://signin.ccci.org/cas",
-  :login_url => "https://signin.ccci.org/cas/login",
-  :validate_url => "https://signin.ccci.org/cas/serviceValidate"
+  :cas_base_url => "https://signin.relaysso.org/cas",
+  :login_url => "https://signin.relaysso.org/cas/login",
+  :validate_url => "https://signin.relaysso.org/cas/serviceValidate"
 )
