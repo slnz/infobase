3e512d8cc354901d19fde6cdbb58bbf701727a51
Weston Platter <westonplatter@gmail.com>
2012-08-22 10:01:27 -0400

    working on spcs for auth

--- spec/models/infobase_user_spec.rb
+++ spec/models/infobase_user_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
-require 'infobase_user'
 
 describe InfobaseUser do
-  it "authenticates" do
-  	
+	before(:each) do
+		@person = Person.create!(:firstName => 'first')
+		@user = @person.create_user(username: 'first.last@gmail.com')
+		@user.person = @person
+		@person.create_staff(accountNo: 123456)
+	end
+  describe "auth" do
+  	it "via StaffsiteProfile" do
+	  	StaffsiteProfile.create!(userName: @user.username)
+	  	info = InfobaseUser.determine_infobase_user(@user, nil)
+  	end
+  end
+
+  xit "auth - hr or admin" do
+  end
+
+  xit "auth - if person not removed from peopleSoft" do
+  end
+
+  xit "auth - person on team" do
   end
 end
