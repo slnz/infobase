5301e84c9470b0c5cdc70e196900bfc180c557ed
Josh Starcher <josh.starcher@gmail.com>
2013-04-15 15:36:53 -0400

    Make sure we have the cas url

--- config/initializers/gcx_api.rb
+++ config/initializers/gcx_api.rb
@@ -1,4 +1,6 @@
 GcxApi.config do |c|
+  c.gcx_url = Rails.configuration.gcx_url
+  c.cas_url = Rails.configuration.cas_url
   c.cas_username = Rails.configuration.cas_username
   c.cas_password = Rails.configuration.cas_password
 end
