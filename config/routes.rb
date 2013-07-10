33e9479c93c1776a832c28a1d8e9631035d15866
Justin Sabelko <justin.sabelko@cojourners.com>
2013-07-10 16:15:52 -0500

    Let's try a POST

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Infobase::Application.routes.draw do
       resources :stats do
         collection do
           get :activity
-          get :collate_stats
+          post :collate_stats
         end
       end
       
