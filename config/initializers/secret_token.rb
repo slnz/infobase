d94b63b0919e6c18f747f5872f502c61f1ae85b2
Justin Sabelko <justin.sabelko@cojourners.com>
2013-10-07 14:19:32 -0500

    Added secret_key_base; Extracted secrets to config file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Infobase::Application.config.secret_token = '2c228bada4a92ae2a781bb58ef1857c507ab1f4aac15e6d7c4cf5e4b82caa185568165f6ad1eb5033dffcc9d8432aa0625ab7b70afa0d27782d5c6a8d1d7b8b4'
+Infobase::Application.config.secret_token = Rails.configuration.secret_token
+Infobase::Application.config.secret_key_base = Rails.configuration.secret_key_base
