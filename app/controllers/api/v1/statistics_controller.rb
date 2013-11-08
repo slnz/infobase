9df0ed8e007b597e3c2acb18bf8d18afae8c80c3
Justin Sabelko <justin.sabelko@cojourners.com>
2013-11-08 12:10:21 -0600

    Fixes to print.css

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -55,7 +55,7 @@ class Api::V1::StatisticsController < Api::V1::BaseController
     report = InfobaseReport.create_report_intervals(begin_date, end_date, activity_ids, semester_needed, interval)
 
     response = {}
-    period_end_date = begin_date.end_of_week(:sunday) + (interval - 1).weeks
+    period_end_date = begin_date.end_of_week(:sunday) + (interval - 1).weeks # first interval includes 1st week by default, need to subtract a week
     temp_row = InfobaseReportRow.new(period_end_date.to_s)
     report.rows.each do |row|
       date = Date.parse(row.name)
