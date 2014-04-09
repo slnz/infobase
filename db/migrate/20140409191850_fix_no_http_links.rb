77e04768ad0a0555353daf7eeaec0f53eec5a5b4
Andrew Roth <andrewroth@gmail.com>
2014-04-09 18:48:11 -0400

    use update_column instead of update_all
    it's cleaner and also doesn't run hooks or validations

--- db/migrate/20140409191850_fix_no_http_links.rb
+++ db/migrate/20140409191850_fix_no_http_links.rb
@@ -4,8 +4,8 @@ class FixNoHttpLinks < ActiveRecord::Migration
       [ :url, :urlToLogo, :ciaUrl, :infoUrl ].each do |c|
         val = ta.send(c)
         if val.present? && !val.starts_with?("http://") && !val.starts_with?("https://")
-          puts "#{ta.id} #{val} -> http://#{val}"
-          TargetArea.where(TargetArea.primary_key => ta.id).update_all(c => "http://#{val}")
+          puts "TargetArea #{ta.id} #{val} -> http://#{val}"
+          ta.update_column(c, "http://#{val}")
         end
       end
     end
@@ -13,8 +13,8 @@ class FixNoHttpLinks < ActiveRecord::Migration
       [ :url ].each do |c|
         val = a.send(c)
         if val.present? && !val.starts_with?("http://") && !val.starts_with?("https://")
-          puts "#{a.id} #{val} -> http://#{val}"
-          TargetArea.where(TargetArea.primary_key => a.id).update_all(c => "http://#{val}")
+          puts "Activity #{a.id} #{val} -> http://#{val}"
+          a.update_column(c, "http://#{val}")
         end
       end
     end
