b632f28d758a2ecb43b831d1d3b5b7ccaf892bef
Josh Starcher <josh.starcher@gmail.com>
2014-08-25 10:15:35 -0400

    Revert change that was causing an infinite serializer loop.

--- app/serializers/target_area_serializer.rb
+++ app/serializers/target_area_serializer.rb
@@ -2,5 +2,5 @@ require_dependency 'cru_enhancements'
 
 class TargetAreaSerializer < ActiveModel::Serializer
   attributes :id, :name, :type, :longitude, :latitude, :enrollment, :region, :state
-  has_many :teams
+  include CruEnhancements
 end
